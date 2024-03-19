ac69ed3d72e664ffe8416c3ec5b862ae5503558a
Luis de Moura <sleepygoatg@gmail.com>
2024-03-19 20:47:54 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, session, redirect, url_for, flash, req
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, SelectField, PasswordField
 from wtforms.validators import DataRequired
 from datetime import datetime
 from flask_moment import Moment
@@ -46,10 +46,10 @@ def index():
         session['remote_addr'] = request.remote_addr;
         session['host'] = request.host;
         return redirect(url_for('index'))
-    
-    return render_template('index.html', 
-                           form=form, 
-                           name=session.get('name'), 
+
+    return render_template('index.html',
+                           form=form,
+                           name=session.get('name'),
                            surname=session.get('surname'),
                            institution=session.get('institution'),
                            discipline=session.get('discipline'),
@@ -57,3 +57,22 @@ def index():
                            remote_addr=session.get('remote_addr'),
                            remote_host=session.get('host'),
                            current_time=datetime.utcnow())
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'POST':
+
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        return redirect(url_for('loginResponse', username=username))
+
+    return render_template('login.html', current_time=datetime.utcnow())
+
+
+@app.route('/loginResponse')
+def loginResponse():
+    username = request.args.get('username')
+    return render_template('loginResponse.html', username=username, current_time=datetime.utcnow())
